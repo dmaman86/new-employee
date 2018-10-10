0c18d3d9cb554e63da1b9c912f904dbfe252e6be
David Maman <dmaman86@gmail.com>
2018-10-10 13:09:29 +0300

    Not Found with dirname to distgit status

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 // Index Route
 
 app.get('*', (req, res) => {
-    res.sendFile( path.join( __dirname, 'angular-src/dist/index.html'))
+    res.sendFile( path.join( __dirname, 'angular-src/dist/angular-src/index.html'))
 })
 
 // cors
